23690eade05a51b064fa8701d5dca126cdf8e50a
Gabriel Rinaldi <gaberinaldi.bio@gmail.com>
2023-07-24 18:04:17 -0700

    final-js

--- script.js
+++ script.js
@@ -6,14 +6,13 @@ $(document).ready(function () {
 
   var hour = dayjs().hour();
   console.log(hour);
-   //localStorage.setItem("currentTime", hour); 
 
   $('.saveBtn').on('click', function() {
     var userInputs = $(this).siblings('.description').val();
-    localStorage.setItem("userSaves", userInputs);
+    var currentTime = $(this).parent().attr('id');
+    localStorage.setItem(currentTime, JSON.stringify(userInputs));
   }) ;
 
-//    console.log($('.time-block'))
     $('.time-block').each(function(i, timeBlock) {
       var timeBlockHour = parseInt($(timeBlock).attr('id').split('-')[1]);
       console.log(timeBlockHour);
@@ -34,12 +33,16 @@ $(document).ready(function () {
       }
     })
 
-    $(window).on('DOMContentLoaded', function() {
-      var storedValue = localStorage.getItem("userSaves");
-      return storedValue;
-      });
-  
-   
+    $('#hour-9 .description').val(localStorage.getItem('hour-9')); 
+    $('#hour-10 .description').val(localStorage.getItem('hour-10')); 
+    $('#hour-11 .description').val(localStorage.getItem('hour-11')); 
+    $('#hour-12 .description').val(localStorage.getItem('hour-12')); 
+    $('#hour-13 .description').val(localStorage.getItem('hour-13')); 
+    $('#hour-14 .description').val(localStorage.getItem('hour-14')); 
+    $('#hour-15 .description').val(localStorage.getItem('hour-15')); 
+    $('#hour-16 .description').val(localStorage.getItem('hour-16')); 
+    $('#hour-17 .description').val(localStorage.getItem('hour-17')); 
+ 
    });
 
 
